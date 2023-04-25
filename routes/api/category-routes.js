c11beb028588438e29d8a40d88b87cc9518b498c
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-04-24 23:58:05 -0500

    Fixed index.js in models directory

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,7 +24,7 @@ try {
             include: [{ model: Product }]
         });
         if (!categoryData) {
-            res.status(404).json({ message: 'No category found with this id' });
+            res.status(400).json({ message: 'No category found with this id' });
             return;
         }
         res.status(200).json(categoryData);
