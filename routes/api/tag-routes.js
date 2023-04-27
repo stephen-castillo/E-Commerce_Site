ea188c25c90599933fa34b7e44515c2ecf56b09c
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-04-26 19:39:05 -0500

    Added missing tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,16 +33,44 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+        const newTag = await Tag.create({
+            tag_name: req.body.tag_name
+        });
+        res.status(200).json(newTag);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+    try {
+        const updatedTag = await Tag.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(updatedTag);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+    try {
+        const deletedTag = await Tag.destroy({
+            where: {
+                id: req.params.id
+            }
+        });
+        res.status(200).json(deletedTag);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
